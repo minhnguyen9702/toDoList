398019ca4710f1726f8b70e82efcb48a5625ac67
minhnguyen9702 <minhnguyen9702@gmail.com>
2024-06-07 17:07:50 +0700

    add modal menu for adding tasks

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-import './style.css'
+import "./style.css"
+import { initializeModal } from './modal';
+
+initializeModal();
+
+class Task {
+    constructor(title, desc, dueDate, priority) {
+        this.title = title;
+        this.desc = desc;
+        this.dueDate = dueDate,
+        this.priority = priority;
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+    getDesc() {
+        return this.desc;
+    }
+
+    getDueDate() {
+        return this.dueDate;
+    }
+
+    getPriority() {
+        return this.priority;
+    }
+}
+
