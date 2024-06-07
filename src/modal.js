0a409276945feef0aab2093f3a42848ed72a288d
minhnguyen9702 <minhnguyen9702@gmail.com>
2024-06-07 18:17:17 +0700

    add forms to modal menu

--- src/modal.js
+++ src/modal.js
@@ -2,6 +2,11 @@ export function initializeModal() {
     var modal = document.getElementById("myModal");
     var btn = document.getElementById("addTask");
     var span = document.getElementsByClassName("close")[0];
+    var titleInput = document.getElementById("title");
+    var priorityInput = document.getElementById("priority");
+    var dateInput = document.getElementById("date");
+    var descInput = document.getElementById("desc");
+    var submit = document.getElementById("submitTask");
   
     btn.onclick = function() {
       modal.style.display = "block";
@@ -9,6 +14,10 @@ export function initializeModal() {
   
     span.onclick = function() {
       modal.style.display = "none";
+    }
+
+    submit.onclick = function() {
+        modal.style.display = "none";
     }
   
     window.onclick = function(event) {
